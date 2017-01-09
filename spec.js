2c49b2b8e821022e8e25306b00a3a751e0f0c4eb
Evgeny Poberezkin <e.poberezkin@me.com>
2017-01-09 21:02:08 +0000

    test(stringify): different options

--- spec.js
+++ spec.js
@@ -18,7 +18,8 @@ describe('stringify', () => {
           "bar": 1
         },
         {
-          "baz": 2
+          "baz": 2,
+          "quux": 3
         }
       ]
     };
@@ -29,13 +30,13 @@ describe('stringify', () => {
     assert.deepEqual(pointers, {
       '': {
         value: { line: 0, column: 0, pos: 0 },
-        valueEnd: { line: 9, column: 1, pos: 73 }
+        valueEnd: { line: 10, column: 1, pos: 90 }
       },
       '/foo': {
         key: { line: 1, column: 2, pos: 4 },
         keyEnd: { line: 1, column: 7, pos: 9 },
         value: { line: 1, column: 9, pos: 11 },
-        valueEnd: { line: 8, column: 3, pos: 71 }
+        valueEnd: { line: 9, column: 3, pos: 88 }
       },
       '/foo/0': {
         value: { line: 2, column: 4, pos: 17 },
@@ -49,13 +50,35 @@ describe('stringify', () => {
       },
       '/foo/1': {
         value: { line: 5, column: 4, pos: 45 },
-        valueEnd: { line: 7, column: 5, pos: 67 }
+        valueEnd: { line: 8, column: 5, pos: 84 }
       },
       '/foo/1/baz': {
         key: { line: 6, column: 6, pos: 53 },
         keyEnd: { line: 6, column: 11, pos: 58 },
         value: { line: 6, column: 13, pos: 60 },
         valueEnd: { line: 6, column: 14, pos: 61 }
+      },
+      "/foo/1/quux": {
+        "key": {
+          "column": 6,
+          "line": 7,
+          "pos": 69
+        },
+        "keyEnd": {
+          "column": 12,
+          "line": 7,
+          "pos": 75
+        },
+        "value": {
+          "column": 14,
+          "line": 7,
+          "pos": 77
+        },
+        "valueEnd": {
+          "column": 15,
+          "line": 7,
+          "pos": 78
+        }
       }
     });
   });
@@ -135,6 +158,113 @@ describe('stringify', () => {
     });
   });
 
+  it('should return undefined if data is not a valid type', () => {
+    assert.strictEqual(jsonMap.stringify(undefined), undefined);
+    assert.strictEqual(jsonMap.stringify(function(){}), undefined);
+    assert.strictEqual(jsonMap.stringify(Symbol()), undefined);
+  });
+
+  it('should generate JSON without whitespace', () => {
+    var data = {
+      foo: [
+        {
+          bar: 1
+        }
+      ]
+    };
+
+    var { json, pointers } = jsonMap.stringify(data);
+
+    testResult(json, pointers, data);
+    assert.deepStrictEqual(pointers, {
+      '': {
+        value: { line: 0, column: 0, pos: 0 },
+        valueEnd: { line: 0, column: 19, pos: 19 }
+      },
+      '/foo': {
+        key: { line: 0, column: 1, pos: 1 },
+        keyEnd: { line: 0, column: 6, pos: 6 },
+        value: { line: 0, column: 7, pos: 7 },
+        valueEnd: { line: 0, column: 18, pos: 18 }
+      },
+      '/foo/0': {
+        value: { line: 0, column: 8, pos: 8 },
+        valueEnd: { line: 0, column: 17, pos: 17 }
+      },
+      '/foo/0/bar': {
+        key: { line: 0, column: 9, pos: 9 },
+        keyEnd: { line: 0, column: 14, pos: 14 },
+        value: { line: 0, column: 15, pos: 15 },
+        valueEnd: { line: 0, column: 16, pos: 16 }
+      }
+    });
+  });
+
+  it('should skip properties with invalid types', () => {
+    var data = {
+      foo: {
+        bar: null,
+        baz: undefined,
+        quux: function(){},
+        sym: Symbol()
+      }
+    };
+
+    assert.deepStrictEqual(
+      jsonMap.stringify(data),
+      jsonMap.stringify({foo: {bar: null}})
+    );
+  });
+
+  it('should stringify items with invalid types as null', () => {
+    var data = {
+      foo: [
+        null,
+        undefined,
+        function(){},
+        Symbol()
+      ]
+    };
+
+    assert.deepStrictEqual(
+      jsonMap.stringify(data),
+      jsonMap.stringify({foo: [null, null, null, null]})
+    );
+  });
+
+  it('should limit whitespace', function() {
+    var data = {
+      "foo": [
+        {
+          "bar": 1
+        },
+        {
+          "baz": 2,
+          "quux": 3
+        }
+      ]
+    };
+
+    equal([
+      jsonMap.stringify(data),
+      jsonMap.stringify(data, null, -1),
+      jsonMap.stringify(data, null, 0),
+      jsonMap.stringify(data, null, '')
+    ]);
+
+    equal([
+      jsonMap.stringify(data, null, 10),
+      jsonMap.stringify(data, null, 20),
+      jsonMap.stringify(data, null, Array(10 + 1).join(' ')),
+      jsonMap.stringify(data, null, Array(20).join(' '))
+    ]);
+
+    assert.notDeepStrictEqual(
+      jsonMap.stringify(data, null, 10),
+      jsonMap.stringify(data, null, Array(9 + 1).join(' '))
+    );
+  });
+
 
   function testResult(json, pointers, data) {
     assert.deepStrictEqual(JSON.parse(json), data);
@@ -151,5 +281,11 @@ describe('stringify', () => {
         jsonPointer.get(data, ptr) // value
       );
     }
+  }
+
+
+  function equal(objects) {
+    for (var i=1; i<objects.lenght; i++)
+      assert.deepStrictEqual(objects[0], objects[i]);
   }
 });
